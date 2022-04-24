ad2061a42ad452e1131d28a5a6c6260c3d809c6b
Alexandru Chelariu <alexandruchelariu@Alexandrus-MacBook-Air.local>
2022-04-25 00:00:00 +0300

    wsgi server initialized(testing round)

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import os
+import sys
+sys.path.append(os.path.dirname(__file__))
+
 import random
 from ast import Import
-import os
 from ssl import AlertDescription
 from flask import Flask, render_template, request, redirect, url_for, session
 import re
@@ -12,11 +15,11 @@ import hashlib
 from time import sleep
 import ssl
 import urllib3
-from random import randint
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import smtplib
 from email.mime.base import MIMEBase
+import random
 salt = "663ba00c232b3663ba00c232b3e42"
 current_dir = os.getcwd()
 def encrypt_string(string):
@@ -27,20 +30,24 @@ def compare_strings(string1, string2):
     return string1 is string2
 
 def email_me(username, generated_code, user_email):
-    sender_mail = "help.creatiigalatene@gmail.com"
-    password = 'vomgip-mAfzih-7hafty'
-    message = MIMEMultipart("alternative")
-    message["Subject"] = str(username) + ": Schimbare Parola CreatiiGalatene"
-    message["From"] = sender_mail
-    message["To"] = user_email
-    text = "Salut, acest email contine codul de confirmare pentru schimbarea parolei tale de creator desavarsit.\n\n\n" + \
-        str(generated_code)
-    part1 = MIMEText(text, "plain")
-    message.attach(part1)
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-        server.login(sender_mail, password="vomgip-mAfzih-7hafty")
-        server.sendmail(sender_mail, user_email, message.as_string())
+    i = 0
+    while i < 1:
+        sender_mail = "help.creatiigalatene@gmail.com"
+        password = 'vomgip-mAfzih-7hafty'
+        message = MIMEMultipart("alternative")
+        message["Subject"] = str(username) + ": Schimbare Parola CreatiiGalatene"
+        message["From"] = sender_mail
+        message["To"] = user_email
+        text = "Salut, acest email contine codul de confirmare pentru schimbarea parolei tale de creator desavarsit.\n\n\n" + str(generated_code)
+        part1 = MIMEText(text, "plain")
+        message.attach(part1)
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+            server.login(sender_mail, password="vomgip-mAfzih-7hafty")
+            server.sendmail(sender_mail, user_email, message.as_string())
+        i += 1
+    if(i == 1):
+        return 0
 # con_string = r"DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=" + str(current_dir) + "/MainDB.accdb;"
 # conn = pyodbc.connect(con_string)
 # cursor = conn.cursor()
@@ -55,6 +62,12 @@ upcase_char = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
                'Y', 'Z']
 symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '*',
            '(', ')', '-', '_', ',', '<', '.', '>', '/', '?', '"', ':', ';']
+def make_pin(length):
+    combined_list = digits + upcase_char + locase_char + symbols
+    string = ''
+    decrypt_password = string.join(random.sample(combined_list, length))
+    return decrypt_password
+
 app = Flask(__name__)
 @app.route('/')
 
@@ -84,41 +97,39 @@ def LogIn():
         #HUGE ISSUE
         #NO SIBIU BACKEND
         #data = cursor.fetchall()
-        for row in data:
-            if email_input == row[1]:
-                print("mail: " + email_input)
-            else:
-                sleep(2)
-                return render_template("sign-up.html")
+        # for row in data:
+        #     if email_input == row[1]:
+        #         print("mail: " + email_input)
+        #     else:
+        #         sleep(2)
+        #         return render_template("sign-up.html")
     return render_template("log-in.html")
 
 @app.route("/SchimbareParola", methods = ["POST", "GET"])
 def ConfirmareCod():
-    def make_pin(length):
-        combined_list = digits + upcase_char + locase_char + symbols
-        string = ''
-        decrypt_password = string.join(random.sample(combined_list, length))
-        return decrypt_password
     user_email = 'alexchelariu834@gmail.com'
     username = 'Alexandru6041'
-    generated_code = make_pin(randint(13, 20))
+    generated_code = make_pin(15)
     generated_code = str(generated_code).encode('utf-8')
     generated_code_hash = encrypt_string(str(generated_code))
         
     email_me(username, generated_code, user_email)
+    
     if request.method == "POST":
         input_code = request.form["user_input_code"]
         hash_input_code = encrypt_string(input_code)
+        print(hash_input_code + "\n" + generated_code_hash)
+        print(compare_strings(hash_input_code, generated_code_hash))
         if compare_strings(hash_input_code, generated_code_hash) == 1:
             return render_template("change_pass_trueverify.html")
         else:
             return render_template("log-in.html")
         
-    return render_template("change_pass_email_confirm.html")   
+    return render_template("change_pass_email_confirm.html")
 
 @app.route("/passwordTrue")
 def password_true():
      return render_template("change_pass_trueverify.html")
  
 if __name__ == "__main__":
-    app.run(debug = True, port = 5500)
+    app.run(host = '0.0.0.0')
---
+++ creatii-galatene.wsgi
@@ -0,0 +1,4 @@
+from app import app as application
+import sys
+import os
+sys.path.insert(0, "app.py")
