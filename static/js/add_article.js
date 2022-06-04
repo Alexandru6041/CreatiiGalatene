6851cc358bfd4c07037eeadce25d523315e207c1
Alexandru Chelariu <alexandruchelariu@Alexandrus-MacBook-Air.local>
2022-06-05 02:02:14 +0300

    new files from last commit

---
+++ static/js/add_article.js
@@ -0,0 +1,8 @@
+const textarea1 = document.getElementById('context_article');
+textarea1.value = '';
+const textarea2 = document.getElementById("summary_article");
+textarea2.value = '';
+window.addEventListener("load", event=>{
+    textarea1.value = '';
+    textarea2.value = '';
+});
